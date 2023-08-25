4dc2a2b1184d717fcfa9f25ed10dce1ba24a6f70
RubyWave <karol.przy419@gmail.com>
2023-08-25 16:17:54 +0200

    added brosersync

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,40 @@
 const path = require('path');
+//to have CSS in separate files, not in JS
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
 
 module.exports = {
     mode: 'development',
-    plugins: [new MiniCssExtractPlugin()],
-    module: {
-      rules: [
-        {
-          test: /\.s[ac]ss$/i,
-          use: [
-            MiniCssExtractPlugin.loader,
-            // Translates CSS into CommonJS
-            "css-loader",
-            // Compiles Sass to CSS
-            "sass-loader",
-          ],
-        },
-      ],
+    plugins: [
+        new MiniCssExtractPlugin(),
+        new BrowserSyncPlugin({
+            port: 3000,
+            proxy: 'http://learningplace.test/',
+            files: [
+              "**/*.php"
+            ]
+
+        })
+    ],
+    watchOptions: {
+        //time that webpack wait to compile files
+        aggregateTimeout: 1500,
+        ignored: '**/node_modules'
     },
-  };
+    module: {
+        //SCSS comiling
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader"
+                ]
+            }
+        ]
+    }
+};
