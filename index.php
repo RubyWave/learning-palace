eb3265d1706d541ab1b7afc10be57a2b7c1468dc
RubyWave <karol.przy419@gmail.com>
2023-08-25 16:17:54 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+    <main>
+
+        <?php
+        if ( have_posts() ) {
+
+            while ( have_posts() ) {
+                the_post();
+            }
+        }
+        ?>
+    </main>
+
+<?php get_footer();
